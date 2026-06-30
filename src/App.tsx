1a9e6196164264734517dfd9da756aceb3bfe426
Lelien Panda <lelinpanda35@gmail.com>
2026-06-30 13:16:33 +0530

    NovaTab - A browser extension for devs to keep productivity going on.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-[#050816]">
+      <h1 className="text-5xl font-bold text-white">
+        NovaTab
+      </h1>
+    </main>
+  );
+}
